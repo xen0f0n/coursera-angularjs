ca5147b0e16b7850ebf708e255fb834ee4f4e64e
xen0fon <akis.karagian@gmail.com>
2016-12-24 13:01:35 +0200

    solution for module 1, first commit

---
+++ module1_solution/app.js
@@ -0,0 +1,26 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+	$scope.msg = "";
+	$scope.list = "list comma separated dishes you usually have for lunch";
+	
+	$scope.count = function () {
+		var arr = $scope.list.split(',');
+		var num_words = arr.length;
+		// check number of items
+		// if number of items > 3
+		if (num_words >= 3) {
+			$scope.msg = "Too much!";
+		}
+		else {
+			$scope.msg = "Enjoy!";
+		}
+	}
+}
+
+})();
